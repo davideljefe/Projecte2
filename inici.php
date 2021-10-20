d2de882fa830b86dbc3c1a1e795721a0b58cd886
David <eljefazo42@gmail.com>
2021-10-20 18:04:57 +0200

    paginainici.php y fitxer css

---
+++ inici.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>HOTEL ABP</title>
+    <link type="text/css" rel="stylesheet" href="hotelabp.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="header"><h1>HOTEL ABP</h1></div>
+            <div class="sidebar">
+                <ul>
+                    <li>
+                        <a href="#">Habitacions</a>
+                    </li>
+                    <li>
+                        <a href="#">Reserves</a>
+                    </li>
+                    <li>
+                        <a href="#">LOGO</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="main">MAIN</div>
+            <div class="sidebar2">SIDEBAR2</div>
+            <div class="footer">FOOTER</div>
+        </div>
+    
+    </body>
+
+</html>
