eea249b72a4f73bab3c17e48ad94c47e719e2373
sergi <cufisergi24@gmail.com>
2021-10-22 16:24:40 +0200

    footer

--- layout/footer.php
+++ layout/footer.php
@@ -1,5 +1,37 @@
 <div id="footer">
       <div class="container">
-         <p class="text-muted credit">Exemple footer</p>
+         <div class="row">
+            <div class="col-md-4 footer-col">
+                <h3>Dirección</h3>
+                <p>
+                    CABA - Argentina <br />
+                    carrasco 400
+                </p>
+            </div>
+            <div class="col-md-4 footer-col">
+                <h3>Mis redes</h3>
+                <ul class="list-inline">
+                    <li>
+                        <a  target="_blank" href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a  target="_blank" href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                    </li>
+                    <li>
+                        <a  target="_blank" href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a  target="_blank" href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                    </li>
+                    <li>
+                        <a  target="_blank" href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4 footer-col">
+                <h3>Trabajo FreeLancer</h3>
+                <p>Tengo más de X años de experiencia en maquetación web y me apasiona lo que hago!</p>
+            </div>
+        </div>
      </div>
  </div>
