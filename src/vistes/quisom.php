4b24be418d25424412ea31f4e8614d491b1f8a4d
sergi <cufisergi24@gmail.com>
2021-11-08 19:18:01 +0100

    més pàgines

--- src/vistes/quisom.php
+++ src/vistes/quisom.php
@@ -9,6 +9,7 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 		  
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+		<link type="text/css" rel="stylesheet" href="footer.css">
 		<title>SOBRE NOSALTRES</title>
 
     </head>
@@ -18,8 +19,25 @@
 	<header>  
 		<?php include("../src/vistes/header.php")?>
     </header>
+<br>
 
-<h1>QUI SOM?</h1>
+<h1 style="margin-left: 15px;">QUI SOM?</h1>
+
+<br>
+
+<p style="margin-right: 200px; text-align: left; margin-left: 15px;">ABP és un hotel de tres estrelles situat a Figueres, la capital de l'Alt Empordà. En el nostre hotel hi podreu trobar allotjament per un preu econòmic durant totes les èpoques de l'any. Figueres és també la capital de la costa brava, un racó de Catalunya on hi ha un munt d'activitats per fer i llocs per predre's i visitar. Podeu consultar la web oficial de la <a href="https://ca.costabrava.org/"> Costa Brava</a> per a més informació.</p>
+<br>
+<div style="margin-bottom: 50px">
+<figure>
+<img src="img/costa_brava.jpg" width="490px" height="318px">
+<figcaption >Platja de la Gola a Llançà, Costa Brava.</figcaption>
+</figure>
+</div>
+
+<div style="text-align: center; margin-bottom: 50px">
+<h3 style="margin-bottom: 20px;">On trobar-nos?</h3>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2952.2255688067917!2d2.962496514957594!3d42.27370804854392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ba8dd91251e3ff%3A0xe8dfb11cd9cdef78!2sInstitut%20Cendrassos!5e0!3m2!1ses!2ses!4v1636394894652!5m2!1ses!2ses" width="800" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
 
     <footer class="text-center footer-style">
         <?php include("../src/vistes/footer.php"); ?>
