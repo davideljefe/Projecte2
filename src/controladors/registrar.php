ef12b5e09a1169b2b6a4b49e50a05ee63a5b6210
sergi <cufisergi24@gmail.com>
2021-11-10 18:54:02 +0100

    canvis

--- src/controladors/registrar.php
+++ src/controladors/registrar.php
@@ -1,17 +1,17 @@
 <?php
-function ctrlRegistrar($peticio, $resposta, $contenidor)
+function ctrlRegistrar($peticio, $resposta, $contenidor, $nomreg, $cognomreg, $dnireg, $emailreg, $passwordreg, $adrecareg)
 {
+	$nomreg = $_POST["nomreg"];   
+	$cognomreg = $_POST["cognomreg"]; 
+	$emailreg = $_POST["emailreg"]; 
+	$dnireg = $_POST["dnireg"];
+	$emailreg = $_POST["emailreg"];
+	$passwordreg = $_POST["passwordreg"];
+	$adrecareg = $_POST["adrecareg"];
+	
     $resposta->SetTemplate("registrar.php");
 
     return $resposta;
 
-
-$nomreg = $_POST["nomreg"];   
-$cognomreg = $_POST["cognomreg"]; 
-$emailreg = $_POST["emailreg"]; 
-$dnireg = $_POST["dnireg"];
-$emailreg = $_POST["emailreg"];
-$passwordreg = $_POST["passwordreg"];
-$adrecareg = $_POST["adrecareg"];
 }
 ?>
