5faa2c21299beb7e1aff594d9a3475c3c4ab8dd1
David <eljefazo42@gmail.com>
2021-11-18 19:44:31 +0100

    millora gestio usuaris

--- src/controladors/identificar.php
+++ src/controladors/identificar.php
@@ -2,6 +2,19 @@
 
 function ctrlIdentificar($peticio, $resposta, $contenidor)
 {
+
+    $usuari = $peticio->get("SESSION", "nom");
+    $pass = $peticio->get("SESSION", "contrasenya");
+    if (!isset($nom)) {
+        $nom = "";
+    }
+    if (!isset($pass)) {
+        $pass = "";
+    }
+
+    $resposta->set("nom", $usuari);
+    $resposta->set("contrasenya", $pass);
+
     $resposta->SetTemplate("identificar.php");
     return $resposta;
 }
